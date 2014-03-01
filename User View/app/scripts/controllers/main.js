26c841a0d3b68561eb11d3952d6df9a031ccfe21
Kai <im.kaiyu@gmail.com>
2014-03-01 17:17:27 -0500

    fixed up view page

--- User View/app/scripts/controllers/main.js
+++ User View/app/scripts/controllers/main.js
@@ -1,11 +1,26 @@
 'use strict';
 
 angular.module('userViewApp')
-  .controller('MainCtrl', function ($scope, $firebase) {
+  .controller('MainCtrl', function ($scope, $firebase, $rootScope) {
     var menu = new Firebase('https://menuapp.firebaseio.com/MenuItems');
     var categories = new Firebase('https://menuapp.firebaseio.com/Category');
 
-    $scope.menuItems = $firebase(menu);
-    $scope.categories = $firebase(categories);
+    menu.on('value', function(dataSnapshot) {
+        $scope.menuItems = dataSnapshot.val();
+      });
 
+    $scope.categories = $firebase(categories);
+    
+    $scope.menuItemArray = (function() {
+    	var itemArray = [];
+    	for(var item in $scope.menuItems) {
+    		itemArray.push($scope.menuItems[item]);
+    	}
+    	return itemArray;
+    })();
+
+    $scope.chooseCategory = function(categ) {
+    	$rootScope.categoryChoice = categ.name;
+    	console.log($scope.categoryChoice)
+	};
   });
